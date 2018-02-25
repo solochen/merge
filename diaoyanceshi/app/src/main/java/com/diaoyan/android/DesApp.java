9746b2422657cc128bbdba74a33bdcb544dc8a85
shaolong <82907183@qq.com>
2018-02-25 11:29:35 +0800

    删除okGo 框架

--- diaoyanceshi/app/src/main/java/com/diaoyan/android/DesApp.java
+++ diaoyanceshi/app/src/main/java/com/diaoyan/android/DesApp.java
@@ -1,11 +1,6 @@
 package com.diaoyan.android;
 
 import android.app.Application;
-import com.diaoyan.android.crashoptimizition.CrashActivity;
-import com.sololibrary.library.http.OkGoHttp;
-import com.sololibrary.library.utils.NetUtil;
-
-import cat.ereza.customactivityoncrash.config.CaocConfig;
 
 /**
  * Created by chenshaolong on 2017/11/5.
@@ -19,18 +14,5 @@ public class DesApp extends Application {
     public void onCreate() {
         super.onCreate();
         app = this;
-        CaocConfig.Builder.create()
-                .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //default: CaocConfig.BACKGROUND_MODE_SHOW_CUSTOM
-                .enabled(true) //default: true
-                .showErrorDetails(true) //default: true
-                .showRestartButton(false) //default: true
-                .trackActivities(true) //default: false
-                .minTimeBetweenCrashesMs(2000) //default: 3000
-                .restartActivity(MainActivity.class) //default: null (your app's launch activity)
-                .errorActivity(CrashActivity.class) //default: null (default error activity)
-                .eventListener(null) //default: null
-                .apply();
-
-        OkGoHttp.INSTANCE.initOkGo(this);
     }
 }
